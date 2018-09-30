744abba98519aaad194daabaf63ad4ade3d6e5a4
dimpy agarwal <dimpyagarwal03@gmail.com>
2018-09-30 23:50:40 +0100

    Home and Register Components added.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { NEXT } from '@angular/core/src/render3/interfaces/view';
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +10,27 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
   login() {
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      console.log('Logged in Successfully');
+    }, error => {
+      console.log('Login Failed');
+    });
+  }
+
+  loggedIn() {
+  const token = localStorage.getItem('token');
+  return !!token;
+
+  }
+
+  logOut() {
+    localStorage.removeItem('token');
+    console.log('Logged Out');
   }
 
 }
--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
             _Context = context;
         }
         // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
